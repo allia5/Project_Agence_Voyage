2b740de414871c9ce19c8a0a1c98244bc95b14b2
ramziallia07 <ramziallia07@gmail.com>
2022-04-13 01:03:22 +0200

    change12

---
+++ Project_Agence_Voyage/Managers/Manager_Passanger/IManager_Passanger.cs
@@ -0,0 +1,12 @@
+﻿using Project_Agence_Voyage.Models.Passanger;
+
+namespace Project_Agence_Voyage.Managers.Manager_Passanger
+{
+    public interface IManager_Passanger
+    {
+        public int post_passanger(Passanger passanger);
+        public int delete_pass_by_id(string id);
+        public int get_pass_by_id(string id);
+        public List<Passanger> get_all_passanger();
+    }
+}
---
+++ Project_Agence_Voyage/Managers/Manager_Passanger/Manager_Passanger.cs
@@ -0,0 +1,27 @@
+﻿using Project_Agence_Voyage.Models.Passanger;
+
+namespace Project_Agence_Voyage.Managers.Manager_Passanger
+{
+    public class Manager_Passanger : IManager_Passanger
+    {
+        public int delete_pass_by_id(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Passanger> get_all_passanger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int get_pass_by_id(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int post_passanger(Passanger passanger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
