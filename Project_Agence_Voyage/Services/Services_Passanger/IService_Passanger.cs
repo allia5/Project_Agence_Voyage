3702ef49f6f8cb569e2fbfe35b810ce5b9260e91
ramziallia07 <ramziallia07@gmail.com>
2022-04-14 16:33:33 +0200

    add services

---
+++ Project_Agence_Voyage/Services/Services_Passanger/IService_Passanger.cs
@@ -0,0 +1,12 @@
+﻿using Project_Agence_Voyage.Models.Passanger;
+
+namespace Project_Agence_Voyage.Services.Services_Passanger
+{
+    public interface IService_Passanger
+    {
+        public int check_post_passanger(Passanger passanger);
+        public int check_delete_pass_by_id(string id);
+        public Passanger _check_get_pass_by_id(string id);
+        public List<Passanger> check_get_all_passanger();
+    }
+}
---
+++ Project_Agence_Voyage/Services/Services_Passanger/Service_Passanger.cs
@@ -0,0 +1,27 @@
+﻿using Project_Agence_Voyage.Models.Passanger;
+
+namespace Project_Agence_Voyage.Services.Services_Passanger
+{
+    public class Service_Passanger : IService_Passanger
+    {
+        public int check_delete_pass_by_id(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Passanger> check_get_all_passanger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int check_post_passanger(Passanger passanger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Passanger _check_get_pass_by_id(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
